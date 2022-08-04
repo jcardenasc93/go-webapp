8d417b22176f1959bc76ccc5d07f8d058dbe45bb
jcardenas <jcardenasc93@gmail.com>
2022-08-04 01:04:08 -0500

    Refactor basic hello world

--- main.go
+++ main.go
@@ -6,16 +6,25 @@ import (
 	"net/http"
 )
 
+const serverPort = ":8000"
+
+// Home is the home handler
+func Home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello from GO server")
+}
+
+// About is the about handler
+func About(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is the about page")
+}
+
 func main() {
 	//NOTE: This is the way to handle requests with builtin go libs
-	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello from GO server")
-		log.Println(fmt.Sprintf("Number of written bytes: %d", n))
-		if err != nil {
-			log.Panic(err)
-		}
-	})
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
+
+	log.Println(fmt.Sprintf("Starting server on %s", serverPort))
 
 	//NOTE: Here starts the server
-	http.ListenAndServe(":8000", nil)
+	http.ListenAndServe(serverPort, nil)
 }
