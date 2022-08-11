84acdcfcdd9e6bd5a880419965ffdc3a3ca564ad
jcardenas <jcardenasc93@gmail.com>
2022-08-11 00:06:36 -0500

    Add layout templates

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -8,7 +8,7 @@ import (
 
 // This method parse a given template in the ResponseWriter
 func renderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	parsedTemplate, _ := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.tmpl")
 	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
 		log.Printf("Error parsing template %s:\n", tmpl)
@@ -19,11 +19,11 @@ func renderTemplate(w http.ResponseWriter, tmpl string) {
 // Home is the home handler
 func Home(w http.ResponseWriter, r *http.Request) {
 	// fmt.Fprintf(w, "Hello from GO server")
-	renderTemplate(w, "home.page.html")
+	renderTemplate(w, "home.page.tmpl")
 }
 
 // About is the about handler
 func About(w http.ResponseWriter, r *http.Request) {
 	// fmt.Fprintf(w, "This is the about page")
-	renderTemplate(w, "about.page.html")
+	renderTemplate(w, "about.page.tmpl")
 }
